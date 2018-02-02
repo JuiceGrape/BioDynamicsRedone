9d9eb58006ec45fe57fa87637353d95ba0e6e19b
JuiceGrape <bmentjot@gmail.com>
2018-02-02 16:40:59 +0100

    Added location to pink diamond NBT tags

--- src/main/java/com/juicegrape/biodynamicsredone/items/ItemPinkDiamond.java
+++ src/main/java/com/juicegrape/biodynamicsredone/items/ItemPinkDiamond.java
@@ -1,6 +1,5 @@
 package com.juicegrape.biodynamicsredone.items;
 
-import com.juicegrape.biodynamicsredone.BioDynamicsRedone;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,10 +7,8 @@ import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -24,6 +21,8 @@ public class ItemPinkDiamond extends ItemBase
     private static final String NBTPURITY = "biodyanmicsredone.purity";
     private static final String NBTSIZE = "biodyanmicsredone.size";
     private static final String NBTCAPACITY = "biodyanmicsredone.capacity";
+    private static final String NBTLOCATION = "biodynamicsredone.location";
+    private static final String NBTLOCATIONSET  = "biodynamicsredone.locationSet";
     public ItemPinkDiamond(String name)
     {
         super(name);
@@ -35,8 +34,15 @@ public class ItemPinkDiamond extends ItemBase
         tooltip.add("Purity: " + getPurity(stack));
         tooltip.add("Size: " + getSize(stack));
         tooltip.add("Capacity: " + getCapacity(stack));
+        if (hasLocation(stack))
+        {
+            int[] location = getLocation(stack);
+            tooltip.add("Location set to: " + location[0] + ", " + location[1] + ", " + location[2]);
+        }
+
     }
 
+    @Override
     public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
     {
         if (this.isInCreativeTab(tab))
@@ -45,6 +51,7 @@ public class ItemPinkDiamond extends ItemBase
             setPurity(meh, 10f);
             setCapacity(meh, 50f);
             setSize(meh, 1f);
+            setHasLocation(meh, false);
             items.add(meh);
 
             ItemStack perfect = new ItemStack(this);
@@ -55,6 +62,7 @@ public class ItemPinkDiamond extends ItemBase
         }
     }
 
+    @Override
     public EnumRarity getRarity(ItemStack stack)
     {
         int rarity = 0;
@@ -67,12 +75,54 @@ public class ItemPinkDiamond extends ItemBase
         return EnumRarity.values()[rarity];
     }
 
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack stack)
+    {
+        return hasLocation(stack);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (!player.isSneaking())
+        {
+            return EnumActionResult.PASS;
+        }
+        ItemStack stack = player.getHeldItem(hand);
+        setLocation(stack, pos.getX(), pos.getY(), pos.getZ());
+        setHasLocation(stack, true);
+        return EnumActionResult.SUCCESS;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        if (!playerIn.isSneaking())
+        {
+            return new ActionResult<ItemStack>(EnumActionResult.PASS, stack);
+        }
+        setHasLocation(stack, false);
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
+    }
+
+    public static NBTTagCompound getCompound(ItemStack stack)
+    {
+        if (stack.hasTagCompound())
+        {
+            return stack.getTagCompound();
+        }
+        else
+        {
+            return new NBTTagCompound();
+        }
+    }
 
     public static float getPurity(ItemStack pinkDiamond)
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond && pinkDiamond.hasTagCompound())
         {
-            NBTTagCompound nbt = pinkDiamond.getTagCompound();
+            NBTTagCompound nbt = getCompound(pinkDiamond);
             return nbt.getFloat(NBTPURITY);
         }
 
@@ -83,7 +133,7 @@ public class ItemPinkDiamond extends ItemBase
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond && pinkDiamond.hasTagCompound())
         {
-            NBTTagCompound nbt = pinkDiamond.getTagCompound();
+            NBTTagCompound nbt = getCompound(pinkDiamond);
             return nbt.getFloat(NBTCAPACITY);
         }
 
@@ -94,11 +144,31 @@ public class ItemPinkDiamond extends ItemBase
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond && pinkDiamond.hasTagCompound())
         {
-            NBTTagCompound nbt = pinkDiamond.getTagCompound();
+            NBTTagCompound nbt = getCompound(pinkDiamond);
             return nbt.getFloat(NBTSIZE);
         }
 
         return 0;
+    }
+
+    public static boolean hasLocation(ItemStack pinkDiamond)
+    {
+        if (pinkDiamond.getItem() instanceof ItemPinkDiamond && pinkDiamond.hasTagCompound())
+        {
+            NBTTagCompound nbt = getCompound(pinkDiamond);
+            return nbt.getBoolean(NBTLOCATIONSET);
+        }
+        return false;
+    }
+
+    public static int[] getLocation(ItemStack pinkDiamond)
+    {
+        if (hasLocation(pinkDiamond))
+        {
+            NBTTagCompound nbt = getCompound(pinkDiamond);
+            return nbt.getIntArray(NBTLOCATION);
+        }
+        return new int[] {0, 0, 0};
     }
 
 
@@ -106,15 +176,7 @@ public class ItemPinkDiamond extends ItemBase
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond)
         {
-            NBTTagCompound nbt;
-            if (pinkDiamond.hasTagCompound())
-            {
-                nbt = pinkDiamond.getTagCompound();
-            }
-            else
-            {
-                nbt = new NBTTagCompound();
-            }
+            NBTTagCompound nbt = getCompound(pinkDiamond);
 
             nbt.setFloat(NBTPURITY, purity);
 
@@ -126,15 +188,7 @@ public class ItemPinkDiamond extends ItemBase
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond)
         {
-            NBTTagCompound nbt;
-            if (pinkDiamond.hasTagCompound())
-            {
-                nbt = pinkDiamond.getTagCompound();
-            }
-            else
-            {
-                nbt = new NBTTagCompound();
-            }
+            NBTTagCompound nbt = getCompound(pinkDiamond);
 
             nbt.setFloat(NBTCAPACITY, capacity);
 
@@ -146,21 +200,39 @@ public class ItemPinkDiamond extends ItemBase
     {
         if (pinkDiamond.getItem() instanceof ItemPinkDiamond)
         {
-            NBTTagCompound nbt;
-            if (pinkDiamond.hasTagCompound())
-            {
-                nbt = pinkDiamond.getTagCompound();
-            }
-            else
-            {
-                nbt = new NBTTagCompound();
-            }
+            NBTTagCompound nbt = getCompound(pinkDiamond);
 
             nbt.setFloat(NBTSIZE, size);
 
             pinkDiamond.setTagCompound(nbt);
         }
     }
+
+    public static void setHasLocation(ItemStack pinkDiamond, boolean hasLocation)
+    {
+        if (pinkDiamond.getItem() instanceof ItemPinkDiamond)
+        {
+            NBTTagCompound nbt = getCompound(pinkDiamond);
+
+            nbt.setBoolean(NBTLOCATIONSET, hasLocation);
+
+            pinkDiamond.setTagCompound(nbt);
+        }
+    }
+
+    public static void setLocation(ItemStack pinkDiamond, int x, int y, int z)
+    {
+        if (pinkDiamond.getItem() instanceof ItemPinkDiamond)
+        {
+            NBTTagCompound nbt = getCompound(pinkDiamond);
+
+            nbt.setIntArray(NBTLOCATION, new int[] {x, y, z});
+
+            pinkDiamond.setTagCompound(nbt);
+        }
+    }
+
+
 
 
 }
